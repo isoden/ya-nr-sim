d1b8288d72edcc38fcba68cbbcc1b546863b3b5a
isoden <3771988+isoden@users.noreply.github.com>
2025-07-22 03:11:37 +0900

    feat: install react-spectrum

--- app/root.tsx
+++ app/root.tsx
@@ -1,24 +1,12 @@
-import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
-
-import type { Route } from './+types/root'
 import './app.css'
 
-export const links: Route.LinksFunction = () => [
-	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-	{
-		rel: 'preconnect',
-		href: 'https://fonts.gstatic.com',
-		crossOrigin: 'anonymous',
-	},
-	{
-		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-	},
-]
+import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
+import { defaultTheme, Provider } from '@adobe/react-spectrum'
+import type { Route } from './+types/root'
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="ja">
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -35,7 +23,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-	return <Outlet />
+	return (
+		<Provider theme={defaultTheme} minHeight="100dvh">
+			<Outlet />
+		</Provider>
+	)
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs'
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
@@ -5,4 +6,15 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	ssr: {
+		noExternal: [
+			'@adobe/react-spectrum',
+			'@react-spectrum/*',
+			'@spectrum-icons/*',
+		].flatMap(spec => fs.globSync(spec, { cwd: 'node_modules' })),
+	},
+	// https://github.com/adobe/react-spectrum/discussions/8189#discussioncomment-13059244
+	define: {
+		'process.env': {},
+	}
 })
